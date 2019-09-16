fdc44bdf5babe8fada91ff2061423b79750376ff
sadin97 <sadinpita_97@windowslive.com>
2019-09-16 16:24:14 +0200

    Social network card template

---
+++ src/components/Card.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import reactimg from '../reactimg.jpg';
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import { deepOrange, deepPurple } from '@material-ui/core/colors';
+import Grid from '@material-ui/core/Grid';
+
+import ChatBubbleOutlineOutlinedIcon from '@material-ui/icons/ChatBubbleOutlineOutlined';
+import ShareRoundedIcon from '@material-ui/icons/ShareRounded';
+import FavoriteBorderRoundedIcon from '@material-ui/icons/FavoriteBorderRounded';
+import FavoriteRoundedIcon from '@material-ui/icons/FavoriteRounded';
+
+const useStyles = makeStyles({
+     avatar: {
+          margin: 10,
+     },
+     orangeAvatar: {
+          margin: 10,
+          color: '#fff',
+          backgroundColor: deepOrange[500],
+     },
+     purpleAvatar: {
+          margin: 10,
+          color: '#fff',
+          backgroundColor: deepPurple[500],
+     },
+});
+
+let name = "Sadin Pita";
+let username = "@sadin.pita";
+let date = "Sep 10";
+let title = "Learning React? Start Small.";
+let subtitle = "Can't pry yourself away from the tutorials? The cure is to make tiny little experiment apps.";
+
+export default function Card () {
+     const classes = useStyles();
+
+     return (
+          <div class="card-wrapper">
+               
+               <div class="card-top-info">
+                    <Avatar className={classes.avatar}>S</Avatar>
+                    <div class="card-name">{name}</div>
+                    <div class="card-light-text"><a href="" class="card-user-link">{username}</a> · {date}</div>
+               </div>
+               <div class="card-bottom-wrapper">
+                    <div class="card-title">{title}</div>
+                    <img src={reactimg} class="card-img" alt="" />
+                    <div class="card-bottom">
+                         <div class="link-title">{title}</div>
+                         <div class="link-subtitle">{subtitle}</div>
+                    </div>
+
+                    <div class="card-icons">
+                         <div><ChatBubbleOutlineOutlinedIcon></ChatBubbleOutlineOutlinedIcon></div>
+                         <div class="card-icons-single"><ShareRoundedIcon></ShareRoundedIcon></div>
+                         <div class="card-icons-single"><FavoriteBorderRoundedIcon></FavoriteBorderRoundedIcon></div>
+                    </div>
+               </div>
+          </div>
+          // <Grid container justify="center" alignItems="center">
+          //      <Avatar className={classes.orangeAvatar}>N</Avatar>
+          //      <Avatar className={classes.purpleAvatar}>OP</Avatar>
+          // </Grid>
+     );
+}
